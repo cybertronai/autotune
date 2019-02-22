d9b61430f6bafb2b183ef6b16714e4664f89335b
KazukiOsawa <osawa1021@gmail.com>
2019-02-22 14:35:09 +0800

    Add batchnorm fisher

---
+++ torchcurv/curv/fisher/batchnorm.py
@@ -0,0 +1,19 @@
+from torchcurv.curv import Curvature, DiagCurvature, KronCurvature
+
+
+class FisherBatchNorm(Curvature):
+
+    def __init__(self):
+        pass
+
+
+class DiagFisherBatchNorm(DiagCurvature):
+
+    def __init__(self):
+        pass
+
+
+class KronFisherBatchNorm(KronCurvature):
+
+    def __init__(self):
+        pass
