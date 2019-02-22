93da85e3e31b70df3ab1bc4babccdda363727ef6
KazukiOsawa <osawa1021@gmail.com>
2019-02-22 14:31:38 +0800

    Mod import

--- torchcurv/curv/__init__.py
+++ torchcurv/curv/__init__.py
@@ -1,18 +1,5 @@
-from torchcurv.curv.curvature import Curvature  # NOQA
-from torchcurv.curv.curvature import DiagCurvature  # NOQA
-from torchcurv.curv.curvature import KronCurvature  # NOQA
-
-from torchcurv.curv.hessian.conv import HessianConv2d  # NOQA
-from torchcurv.curv.hessian.conv import DiagHessianConv2d  # NOQA
-from torchcurv.curv.hessian.conv import KronHessianConv2d  # NOQA
-
-from torchcurv.curv.gn.conv import GNConv2d  # NOQA
-from torchcurv.curv.gn.conv import DiagGNConv2d  # NOQA
-from torchcurv.curv.gn.conv import KronGNConv2d  # NOQA
-
-from torchcurv.curv.fisher.linear import FisherLinear  # NOQA
-from torchcurv.curv.fisher.linear import DiagFisherLinear  # NOQA
-from torchcurv.curv.fisher.linear import KronFisherLinear  # NOQA
-from torchcurv.curv.fisher.conv import FisherConv2d  # NOQA
-from torchcurv.curv.fisher.conv import DiagFisherConv2d  # NOQA
-from torchcurv.curv.fisher.conv import KronFisherConv2d  # NOQA
+from torchcurv.curv.curvature import Curvature, DiagCurvature, KronCurvature  # NOQA
+from torchcurv.curv.hessian.conv import HessianConv2d, DiagHessianConv2d, KronHessianConv2d  # NOQA
+from torchcurv.curv.gn.conv import GNConv2d, DiagGNConv2d, KronGNConv2d  # NOQA
+from torchcurv.curv.fisher.linear import FisherLinear, DiagFisherLinear, KronFisherLinear  # NOQA
+from torchcurv.curv.fisher.conv import FisherConv2d, DiagFisherConv2d, KronFisherConv2d  # NOQA
