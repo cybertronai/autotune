9c1be650f72b5acac26987f9fb4426f12b48f36d
KazukiOsawa <osawa1021@gmail.com>
2019-02-23 16:06:12 +0800

    Mod train script for cifar

--- torchcurv/optim/secondorder.py
+++ torchcurv/optim/secondorder.py
@@ -4,7 +4,7 @@ from torchcurv.curv import *
 
 class SecondOrderOptimizer(Optimizer):
 
-    def __init__(self, model):
+    def __init__(self, params, lr=1e-3):
         pass
 
     def add_param_group(self, param_group):
