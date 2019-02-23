25674de72a4a6558ed9d45b305dba710e2c8614e
hn8629 <hn862943@outlook.jp>
2019-02-23 21:42:02 +0900

    first commit to KCurv,KFLinear,KFconv

--- torchcurv/curv/fisher/conv.py
+++ torchcurv/curv/fisher/conv.py
@@ -1,5 +1,8 @@
 from torchcurv.curv import Curvature, DiagCurvature, KronCurvature
+import torch
+import torch.nn.functional as F
 
+from torchcurv.utils import inv
 
 class FisherConv2d(Curvature):
 
@@ -15,5 +18,47 @@ class DiagFisherConv2d(DiagCurvature):
 
 class KronFisherConv2d(KronCurvature):
 
-    def __init__(self):
-        pass
+    def __init__(self, kernel_size, stride, padding, dilation, damping=0.01, cov_ema_decay=0.99, bias=True, pi_type='trace_norm'):
+        super(KronFisherConv2d, self).__init__(damping, cov_ema_decay, bias, pi_type)
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+
+    def compute_G(self, grad_output_data):
+        batch_size, c, h, w = grad_output_data.shape
+        m = grad_output_data.transpose(0, 1).reshape(c, -1)
+
+        return m.mm(m.transpose(0, 1)).mul(1/(batch_size*h*w))
+
+    def compute_A(self, input_data):
+        input_data2d = F.unfold(input_data, kernel_size=self.kernel_size,
+                                stride=self.stride, padding=self.padding, dilation=self.dilation)
+        batch_size, a, _ = input_data2d.shape
+        m = input_data2d.transpose(0, 1).reshape(a, -1)
+        a, b = m.shape
+        if self.bias:
+            m = torch.cat((m, torch.ones((1, b), device=input_data.device)), 0)
+
+        return m.mm(m.transpose(0, 1)).mul(1/batch_size)
+
+    def compute_precgrad(self, tp, kfac_buf):
+        A, G = self.compute_A(), self.compute_G()
+        if self.cov_ema_decay != 0:
+            self.update_covs_ema((A, G))
+            A, G = self.covs_ema
+
+        A, G = self.compute_damped_covs((A, G))
+
+        A_inv, G_inv = inv(A), inv(G)
+
+        param_grad = tp[0].grad
+        oc, ic, h, w = param_grad.shape
+        param_grad2d = param_grad.reshape(oc, -1)
+        if self.bias:
+            param_grad2d = torch.cat((param_grad2d, tp[1].grad.view(-1, 1)), 1)
+            precgrad2d = G_inv.mm(param_grad2d).mm(A_inv)
+            return precgrad2d[:, 0:-1].reshape(oc, ic, h, w), precgrad2d[:, -1]
+        else:
+            precgrad2d = G_inv.mm(param_grad2d).mm(A_inv)
+            return (precgrad2d.reshape(oc, ic, h, w),)
--- torchcurv/curv/fisher/linear.py
+++ torchcurv/curv/fisher/linear.py
@@ -1,5 +1,9 @@
 from torchcurv.curv import Curvature, DiagCurvature, KronCurvature
 
+import torch
+
+from .inv import inv
+
 
 class FisherLinear(Curvature):
 
@@ -17,3 +21,40 @@ class KronFisherLinear(KronCurvature):
 
     def __init__(self):
         pass
+
+    def compute_G(self):
+        grad_output_data = self._module.grad_output_data
+        batch_size = grad_output_data.shape[0]
+        return grad_output_data.transpose(0, 1).mm(grad_output_data).mul(1/batch_size)
+
+    def compute_A(self):
+        input_data = self._module.input_data
+        batch_size = input_data.shape[0]
+        if self.bias:
+            input_data = torch.cat((input_data, torch.ones(
+                (batch_size, 1), device=input_data.device)), 1)
+        return input_data.transpose(0, 1).mm(input_data).mul(1/batch_size)
+
+    def compute_precgrad(self, params):
+        # update covs
+        A, G = self.compute_A(), self.compute_G()
+        self.covs = A, G
+        # update covs_ema
+        if self.cov_ema_decay != 0:
+            self.update_covs_ema()
+            A, G = self.covs_ema
+
+        A, G = self.compute_damped_covs((A, G))
+
+        A_inv, G_inv = inv(A), inv(G)
+
+        # todo check params == list?
+        if self.bias:
+            param_grad = torch.cat(
+                (params[0].grad, params[1].grad.view(-1, 1)), 1)
+            precgrad = G_inv.mm(param_grad).mm(A_inv)
+            return precgrad[:, 0:-1], precgrad[:, -1]
+        else:
+            param_grad = params[0].grad
+            precgrad = G_inv.mm(param_grad).mm(A_inv)
+            return (precgrad,)
