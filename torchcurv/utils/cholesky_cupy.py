75216f83424af3a65bd1543678d98f2f617d22cc
nhac43 <hn862943@outlook.jp>
2019-03-19 21:32:55 +0900

    add cupy.py

--- torchcurv/utils/cholesky_cupy.py
+++ torchcurv/utils/cholesky_cupy.py
@@ -1,12 +1,11 @@
-
 import cupy
 
-from torch.utils.dlpack import to_dlpack, from_dlpack
+from torchcurv.utils.cupy import to_cupy, from_cupy
 
 
 def cholesky(m, upper=True):
-    m_cp = cupy.fromDlpack(to_dlpack(m))
-    chl_cp = cupy.linalg.decomposition.cholesky(m_cp)
+    m_cp = to_cupy(m)
+    m_chl_cp = cupy.linalg.decomposition.cholesky(m_cp)
     if upper:
-        chl_cp = chl_cp.transpose()
-    return from_dlpack(chl_cp.toDlpack())
+        m_chl_cp = m_chl_cp.transpose()
+    return from_cupy(m_chl_cp)
