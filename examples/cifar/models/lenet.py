243be72072cc2353d4d891595665bf200968acb6
KazukiOsawa <osawa1021@gmail.com>
2019-03-19 15:40:56 +0800

    Add BatchNorm LeNet & Mod config for VOGN

--- examples/cifar/models/lenet.py
+++ examples/cifar/models/lenet.py
@@ -21,3 +21,28 @@ class LeNet5(nn.Module):
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
         return out
+
+
+class LeNet5BatchNorm(nn.Module):
+    def __init__(self, num_classes=10, affine=False):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.bn1 = nn.BatchNorm2d(6, affine=affine)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16, affine=affine)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.bn3 = nn.BatchNorm1d(120, affine=affine)
+        self.fc2 = nn.Linear(120, 84)
+        self.bn4 = nn.BatchNorm1d(84, affine=affine)
+        self.fc3 = nn.Linear(84, num_classes)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.bn3(self.fc1(out)))
+        out = F.relu(self.bn4(self.fc2(out)))
+        out = self.fc3(out)
+        return out
