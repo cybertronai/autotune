fce88c7db8740928a1f522baab029bc0794644d4
nhac43 <hn862943@outlook.jp>
2019-03-20 13:22:53 +0900

    fix for pep8 (cupy.py)

--- torchcurv/utils/cupy.py
+++ torchcurv/utils/cupy.py
@@ -5,5 +5,6 @@ from torch.utils.dlpack import to_dlpack, from_dlpack
 def to_cupy(m_tensor):
     return cupy.fromDlpack(to_dlpack(m_tensor))
 
+
 def from_cupy(m_cp):
     return from_dlpack(m_cp.toDlpack())
