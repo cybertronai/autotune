c867e10b2c6aad158ea45749da44d717a9b75fae
KazukiOsawa <osawa1021@gmail.com>
2019-03-20 09:59:11 +0800

    Add DiagFisherBatchNormNd & Mod the way of specifying the curvature class

--- torchcurv/__init__.py
+++ torchcurv/__init__.py
@@ -6,4 +6,4 @@ from torchcurv.curv.hessian.conv import HessianConv2d, DiagHessianConv2d, KronHe
 from torchcurv.curv.gn.conv import GNConv2d, DiagGNConv2d, KronGNConv2d  # NOQA
 from torchcurv.curv.fisher.linear import FisherLinear, DiagFisherLinear, KronFisherLinear  # NOQA
 from torchcurv.curv.fisher.conv import FisherConv2d, DiagFisherConv2d, KronFisherConv2d  # NOQA
-from torchcurv.curv.fisher.batchnorm import FisherBatchNorm2d, DiagFisherBatchNorm2d, KronFisherBatchNorm2d  # NOQA
+from torchcurv.curv.fisher.batchnorm import FisherBatchNorm1d, DiagFisherBatchNorm1d, FisherBatchNorm2d, DiagFisherBatchNorm2d  # NOQA
