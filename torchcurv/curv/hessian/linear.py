6799e9eb23bf1c51403322d2104c89458eeb143d
KazukiOsawa <osawa1021@gmail.com>
2019-04-05 16:18:20 +0800

    Add recursive approximation of hessian

--- torchcurv/curv/hessian/linear.py
+++ torchcurv/curv/hessian/linear.py
@@ -5,7 +5,7 @@ from torchcurv import KronFisherLinear
 class KronHessianLinear(KronFisherLinear):
 
     def __init__(self, module, ema_decay=1., damping=0,
-                 pre_curv=None, post_curv=None, recursive_approx=False):
+                 pre_curv=None, post_curv=None, recursive_approx=True):
         super(KronHessianLinear, self).__init__(module, ema_decay, damping,
                                                 pre_curv=pre_curv, post_curv=post_curv)
         self.recursive_approx = recursive_approx
@@ -35,16 +35,17 @@ class KronHessianLinear(KronFisherLinear):
             assert post_hessian_input is not None, msg
             assert post_grad_input is not None, msg
 
-            recursive_approx = getattr(post_curv, 'recursive_approx', False)
-
             # compute grad of post activation (we support piece wise activation only)
             zero_indices = [grad_output == 0]  # for avoiding division by zero
             grad_activation = post_grad_input.div(grad_output)  # n x f_out
             grad_activation[zero_indices] = 0
             grad_activation_2d = torch.diag_embed(grad_activation)  # n x f_out x f_out
 
+            recursive_approx = getattr(post_curv, 'recursive_approx', False)
+
             # compute sample hessian_output based on hessian_input of post module
-            hessian_output = torch.einsum('bij,bjk,bkl->bil',  # n x f_out x f_out
+            equation = 'bij,jk,bkl->bil' if recursive_approx else 'bij,bjk,bkl->bil'
+            hessian_output = torch.einsum(equation,  # n x f_out x f_out
                                           grad_activation_2d,
                                           post_hessian_input,
                                           grad_activation_2d)
@@ -68,6 +69,9 @@ class KronHessianLinear(KronFisherLinear):
         if self.pre_curv is not None:
             # compute sample hessian_input
             w = self._module.weight.data  # f_out x f_in
-            hessian_input = torch.einsum('ij,bjk,kl->bil', w.t(), hessian_output, w)  # n x f_in x f_in
+            if self.recursive_approx:
+                hessian_input = torch.einsum('ij,jk,kl->il', w.t(), self._G, w)  # n x f_in x f_in
+            else:
+                hessian_input = torch.einsum('ij,bjk,kl->bil', w.t(), hessian_output, w)  # n x f_in x f_in
             setattr(self._module, 'hessian_input', hessian_input)
 
