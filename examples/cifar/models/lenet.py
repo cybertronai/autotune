45753c2fc3be3e647ac837f3cf7aee49ff9b5911
KazukiOsawa <osawa1021@gmail.com>
2019-06-06 14:57:36 +0800

    update configs

--- examples/cifar/models/lenet.py
+++ examples/cifar/models/lenet.py
@@ -1,8 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torchcurv.utils.accumulator import TensorAccumulator
 
 
 class LeNet5(nn.Module):
+
     def __init__(self, num_classes=10):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
@@ -21,6 +23,40 @@ class LeNet5(nn.Module):
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
         return out
+
+
+class LeNet5MCDropout(LeNet5):
+
+    def __init__(self, num_classes=10, dropout_ratio=0.1, val_mc=10):
+        super(LeNet5MCDropout, self).__init__(num_classes=num_classes)
+        self.dropout_ratio = dropout_ratio
+        self.val_mc = val_mc
+
+    def forward(self, x):
+        p = self.dropout_ratio
+        out = F.relu(F.dropout(self.conv1(x), p))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(F.dropout(self.conv2(out), p))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(F.dropout(self.fc1(out), p))
+        out = F.relu(F.dropout(self.fc2(out), p))
+        out = F.dropout(self.fc2(out), p)
+        return out
+
+    def mc_prediction(self, x):
+
+        acc_prob = TensorAccumulator()
+        m = self.val_mc
+
+        for _ in range(m):
+            output = self.forward(x)
+            prob = F.softmax(output, dim=1)
+            acc_prob.update(prob, scale=1/m)
+
+        prob = acc_prob.get()
+
+        return prob
 
 
 class LeNet5BatchNorm(nn.Module):
