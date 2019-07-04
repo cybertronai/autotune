8145f97bf2e0a783f228c3e20b9a78a404e3ada3
KazukiOsawa <osawa1021@gmail.com>
2019-07-04 22:20:13 +0800

    add bnorm diag fisher

--- torchcurv/curv/fisher/linear.py
+++ torchcurv/curv/fisher/linear.py
@@ -1,5 +1,38 @@
 import torch
-from torchcurv import KronCovLinear, Fisher
+from torchcurv import DiagCovLinear, KronCovLinear, Fisher
+
+
+class DiagFisherLinear(DiagCovLinear, Fisher):
+
+    def __init__(self, *args, **kwargs):
+        DiagCovLinear.__init__(self, *args, **kwargs)
+        Fisher.__init__(self)
+
+    def update_in_backward(self, grad_output):
+        if self.do_backward:
+            assert self.prob is not None
+
+            data_input = getattr(self._module, 'data_input', None)  # n x f_in
+            assert data_input is not None
+
+            n = data_input.shape[0]
+
+            in_in = data_input.mul(data_input)  # n x f_in
+
+            pg = torch.mul(grad_output, self.prob.reshape(n, 1))
+            grad_grad = pg.mul(grad_output)  # n x f_out
+
+            data_w = torch.einsum('ki,kj->ij', grad_grad,
+                                  in_in).div(n)  # f_out x f_in
+            self._data = [data_w]
+
+            if self.bias:
+                data_b = grad_grad.mean(dim=0)  # f_out x 1
+                self._data.append(data_b)
+
+            self.accumulate_cov(self._data)
+        else:
+            self._data = self.finalize()
 
 
 class KronFisherLinear(KronCovLinear, Fisher):
