92c18858a7c8c2bfd76505b673e77084091bca39
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-08-09 08:43:27 +0900

    1-mc code

--- torchcurv/curv/fisher/__init__.py
+++ torchcurv/curv/fisher/__init__.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 import torch.nn.functional as F
 
 from torchcurv.utils import TensorAccumulator
@@ -28,7 +29,11 @@ class Fisher(object):
         return self._acc_cov.get()
 
 
-def get_closure_for_fisher(optimizer, model, data, target):
+def get_closure_for_fisher(optimizer, model, data, target, mc_approx=False, num_mc=1):
+
+    def cross_entropy(logits, soft_targets):
+        logsoftmax = nn.LogSoftmax(dim=1)
+        return torch.mean(torch.sum(-soft_targets * logsoftmax(logits), 1))
 
     def closure():
         optimizer.zero_grad()
@@ -41,11 +46,21 @@ def get_closure_for_fisher(optimizer, model, data, target):
             for param in group['params']:
                 param.requires_grad = False
 
-        for i in range(model.num_classes):
+        if mc_approx:
+            dist = torch.distributions.Dirichlet(prob)
+            soft_target = dist.sample((num_mc,))
             for group in optimizer.param_groups:
-                group['curv'].prob = prob[:, i]
-            loss = F.cross_entropy(output, torch.ones_like(target).mul(i))
-            loss.backward(retain_graph=True)
+                group['curv'].prob = torch.ones_like(prob[:, 0]) * (1/num_mc)
+
+            for i in range(num_mc):
+                loss = cross_entropy(output, soft_target[i])
+                loss.backward(retain_graph=True)
+        else:
+            for i in range(model.num_classes):
+                for group in optimizer.param_groups:
+                    group['curv'].prob = prob[:, i]
+                loss = F.cross_entropy(output, torch.ones_like(target).mul(i))
+                loss.backward(retain_graph=True)
 
         for group in optimizer.param_groups:
             group['curv'].turn_off_backward()
