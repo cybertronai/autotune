a623899ad170a872566ac01f3e982cd6844a1410
KazukiOsawa <osawa1021@gmail.com>
2019-08-13 11:02:55 +0800

    mod mc sampling for Fisher

--- torchcurv/curv/fisher/__init__.py
+++ torchcurv/curv/fisher/__init__.py
@@ -1,5 +1,3 @@
-import random
-
 import torch
 import torch.nn.functional as F
 
@@ -30,48 +28,50 @@ class Fisher(object):
         return self._acc_cov.get()
 
 
-def get_closure_for_fisher(optimizer, model, data, target, sample_type=None, num_samples=None):
+def get_closure_for_fisher(optimizer, model, data, target, mc_approx=False, num_mc=1):
 
-    def closure():
-        optimizer.zero_grad()
-        output = model(data)
-        prob = F.softmax(output, dim=1)
-        n = data.shape[0]
-
+    def turn_off_param_grad():
         for group in optimizer.param_groups:
             assert isinstance(group['curv'], Fisher)
             group['curv'].turn_on_backward()
             for param in group['params']:
                 param.requires_grad = False
 
-        classes = range(model.num_classes)
-
-        if sample_type is None or num_samples is None:
-            stargets = [torch.ones_like(target).mul(i) for i in classes]
-            sprob = [prob[:, i] for i in classes]
-        else:
-            assert num_samples <= model.num_classes
-            if sample_type == 'mc':
-                sidx = [random.sample(classes, k=num_samples) for _ in range(n)]
-                stargets = target.new_tensor(sidx).transpose(0, 1)
-                tensors = tuple(p[idx] for p, idx in zip(prob, sidx))
-                sprob = torch.stack(tensors).transpose(0, 1)
-            elif sample_type == 'topk':
-                sprob, stargets = torch.topk(prob, k=num_samples)
-                sprob, stargets = sprob.transpose(0, 1), stargets.transpose(0, 1)
-            else:
-                raise ValueError
-
-        for p, t in zip(sprob, stargets):
-            for group in optimizer.param_groups:
-                group['curv'].prob = p
-            loss = F.cross_entropy(output, t)
-            loss.backward(retain_graph=True)
-
+    def turn_on_param_grad():
         for group in optimizer.param_groups:
             group['curv'].turn_off_backward()
             for param in group['params']:
                 param.requires_grad = True
+
+    def closure():
+
+        def cross_entropy(logits, soft_targets):
+            logsoftmax = torch.nn.LogSoftmax(dim=1)
+            return torch.mean(torch.sum(-soft_targets * logsoftmax(logits), 1))
+
+        optimizer.zero_grad()
+        output = model(data)
+        prob = F.softmax(output, dim=1)
+
+        turn_off_param_grad()
+
+        if mc_approx:
+            dist = torch.distributions.Dirichlet(prob)
+            soft_target = dist.sample((num_mc,))
+            for group in optimizer.param_groups:
+                group['curv'].prob = torch.ones_like(prob[:, 0]).div(num_mc)
+
+            for i in range(num_mc):
+                loss = cross_entropy(output, soft_target[i])
+                loss.backward(retain_graph=True)
+        else:
+            for i in range(model.num_classes):
+                for group in optimizer.param_groups:
+                    group['curv'].prob = prob[:, i]
+                loss = F.cross_entropy(output, torch.ones_like(target).mul(i))
+                loss.backward(retain_graph=True)
+
+        turn_on_param_grad()
 
         loss = F.cross_entropy(output, target)
         loss.backward()
