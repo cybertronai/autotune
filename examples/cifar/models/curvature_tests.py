bd56340359f35a28ea1e5bae01e07083b80f1da1
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-08-16 21:55:57 -0700

    unit tests for relevant batch size formulas

--- examples/cifar/models/curvature_tests.py
+++ examples/cifar/models/curvature_tests.py
@@ -1,12 +1,13 @@
 # Prototype batch-size quantities from
-# https://docs.google.com/document/d/19Jmh4spbSAnAGX_eq7WSFPgLzrpJEhiZRpjX1jSYObo/edit
+# Batch size formulas (https://docs.google.com/document/d/19Jmh4spbSAnAGX_eq7WSFPgLzrpJEhiZRpjX1jSYObo/edit)
+from typing import Optional, Tuple, Callable
 
-import numpy as np
+import scipy
 import torch
 import torch.nn as nn
-
 from torchcurv.optim import SecondOrderOptimizer
 
+import numpy as np
 
 class Net(nn.Module):
     def __init__(self, d):
@@ -14,8 +15,8 @@ class Net(nn.Module):
         self.w = nn.Linear(d, 1, bias=False)
 
     def forward(self, x: torch.Tensor):
-        return self.w(x)
-
+        result = self.w(x)
+        return result
 
 def test_loss():
     # Reproduce Linear Regression example
@@ -24,7 +25,10 @@ def test_loss():
     d = 2
     n = 3
     model = Net(d)
-    model.w.weight.data.copy_(torch.from_numpy(np.array([[1, 2]])))
+
+    w0 = torch.tensor([[1, 2]]).float()
+    assert w0.shape[1] == d
+    model.w.weight.data.copy_(w0)
 
     X = torch.tensor([[-2, 0, 2], [-1, 1, 3]]).float()
     assert X.shape[0] == d
@@ -33,14 +37,12 @@ def test_loss():
     Y = torch.tensor([[0, 1, 2]]).float()
     assert Y.shape[1] == X.shape[1]
 
-    w0 = torch.tensor([[1, 2]])
-    assert w0.shape[1] == d
-
     data = X.t()       # PyTorch expects batch dimension first
     target = Y.t()
-    assert len(data) == n
+    assert data.shape[0] == n
 
     output = model(data)
+    # residuals, aka e
     residuals = output - Y.t()
 
     def compute_loss(residuals_):
@@ -53,59 +55,162 @@ def test_loss():
     print(loss)
 
     # use learning rate 0 to avoid changing parameter vector
-    optim_kwargs = dict(lr=0, curv_type="Fisher", curv_shapes={"Linear": "Kron"}, momentum=0,
-                        momentum_type="preconditioned", weight_decay=0)
-    curv_args = dict(damping=0, ema_decay=1)
+    optim_kwargs = dict(lr=0,  momentum=0, weight_decay=0, l2_reg=0,
+                        bias_correction=False, acc_steps=1,
+                        curv_type="Cov", curv_shapes={"Linear": "Kron"},
+                        momentum_type="preconditioned", )
+    curv_args = dict(damping=1, ema_decay=1)  # todo: damping
     optimizer = SecondOrderOptimizer(model, **optim_kwargs, curv_kwargs=curv_args)
 
-    # update params
-    def closure_fisher():
-        """Closure for estimating empirical Fisher"""
-        for group in optimizer.param_groups:
-            group['curv'].prob = torch.ones_like(Y)  # q: what is prob for?
-            group['curv'].turn_off_backward()
-        optimizer.zero_grad()
-        output_ = model(data)
-        loss_ = compute_loss(output_ - Y.t())
-        loss_.backward(create_graph=False)
+    # def set_requires_grad(v):
+    #     for p in model.parameters():
+    #         p.requires_grad = False
+    #
+    def backward(last_layer: str) -> Callable:
+        """Creates closure that backpropagates either from output layer or from loss layer"""
+        def closure() -> Tuple[Optional[torch.Tensor], torch.Tensor]:
+            optimizer.zero_grad()
+            output = model(data)
+            if last_layer == "output":
+                output.backward(torch.ones_like(target))
+                return None, output
+            elif last_layer == 'loss':
+                loss = compute_loss(output - target)
+                loss.backward()
+                return loss, output
+            else:
+                assert False, 'last layer must be "output" or "loss"'
+        return closure
 
-        return loss_, output_
+    #    loss = compute_loss(output - Y.t())
+    #    loss.backward()
 
-    num_mc = 1
+    loss, output = optimizer.step(closure=backward('loss'))
+    check_close(output.t(), [[-4, 2, 8]])
+    check_close(residuals.t(), [[-4, 1, 6]])
+    check_close(loss, 8.833333)
 
-    def closure_hessian():
-        """Closure for estimating Hessian"""
-        optimizer.zero_grad()
-        output_ = model(data)
+    # batch output Jacobian
+    J = X.t()
+    check_close(J, [[-2, -1], [0, 1], [2, 3]])
 
-        for group in optimizer.param_groups:
-            group['curv'].prob = torch.ones_like(Y)
-            group['curv'].turn_on_backward()
-            for param in group['params']:
-                param.requires_grad = False
+    # Hessian
+    H = J.t() @ J / n
+    check_close(H, [[2.66667, 2.66667], [2.66667, 3.66667]])
 
-        for group in optimizer.param_groups:
-            group['curv'].prob = torch.ones_like(target)
+    # loss
+    loss2 = (residuals * residuals).sum()/(2*n)
+    check_close(to_scalar(loss2), 8.83333)
 
-            for i in range(num_mc):
-                loss_ = compute_loss(output_ - target)
-                loss_.backward(retain_graph=True)
+    # gradients, n,d
+    G = residuals.repeat(1, d) * J
+    check_close(G, [[8., 4.], [0., 1.], [12., 18.]])
 
-        for group in optimizer.param_groups:
-            group['curv'].turn_off_backward()
-            for param in group['params']:
-                param.requires_grad = True
+    # mean gradient
+    g = G.sum(dim=0)/n
+    check_close(g, [6.66667, 7.66667])
 
-    loss = compute_loss(output - Y.t())
-    loss.backward()
+    # empirical Fisher
+    efisher = G.t() @ G / n
+    check_close(efisher, [[69.3333, 82.6667], [82.6667, 113.667]])
 
-    loss, output = optimizer.step(closure=closure_fisher())
+    # centered empirical Fisher (Sigma in OpenAI paper, estimate of Sigma in Jain paper)
+    sigma = efisher - outer(g, g)
+    check_close(sigma, [[24.8889, 31.5556], [31.5556, 54.8889]])
+
     # TODO: get A's and B's, compute Sigma, Sigma_c, gradient diversity,
+    sigma_norm = torch.norm(sigma)
+    g_norm = torch.norm(g)
 
-    loss, output = optimizer.step(closure=closure_hessian())
+    ### OpenAI quantities
+    g_ = g.unsqueeze(0)   # turn g into row matrix
+    grad_curvature = to_scalar(g_ @ H @ g_.t())   # curvature in direction of g
+    stepOpenAI = to_scalar(g.norm()**2/grad_curvature) if g_norm else 999
+    check_close(stepOpenAI, 0.170157)
+    batchOpenAI = to_scalar(torch.trace(H@sigma)/grad_curvature) if g_norm else 999
+    check_close(batchOpenAI, 0.718603)
+
+    ### Gradient diversity  quantities
+    # gradient diversity
+    diversity = torch.norm(G, "fro")**2 / torch.norm(g)**2
+    check_close(diversity, 5.31862)
+
+    ### Jain/Kakade quantities
+
+    # noise scale (Jain, minimax rate of estimator)
+    noise_variance = torch.trace(H.inverse() @ sigma)
+    check_close(noise_variance, 26.)
+
+    isqrtH = inv_square_root(to_numpy(H))
+    isqrtH = torch.tensor(isqrtH)
+    # measure of misspecification between model and actual noise (Jain, \rho)
+    rho = (d/rank(isqrtH @ sigma @ isqrtH)) if sigma_norm > 0 else 1
+    check_close(rho, 1.4221)
+    assert rho >= 1, rho
+    assert rho <= d, rho
+
+    # divergent learning rate for batch-size 1 (Jain)
+    stepMin = 2/torch.trace(H)
+    check_close(stepMin, 0.315789)
+
+    # divergent learning rate for batch-size infinity
+    stepMax = 2/l2_norm(H)
+    check_close(stepMax, 0.340147)
+
+    # divergent learning rate for batch-size 1, adjusted for misspecification
+    stepMinAdjusted = stepMin/rho
+    check_close(stepMinAdjusted, 0.222058)
+
+    # batch size that gives provides lr halfway between stepMin and stepMax
+    batchJain = 1 + rank(H)
+    check_close(batchJain, 2.07713)
+
+    # batch size that provides halfway point after adjusting for misspecification
+    batchJainAdjusted = 1+rank(H)*rho
+    check_close(batchJainAdjusted, 2.5318)
+
+    loss, output = optimizer.step(closure=backward('output'))
     # TODO: get A's and B's, compute H, rho, Newton decrement, all learning rate + batch size stats
-    print(loss.item())
+    #    print(loss.item())
 
+
+def l2_norm(mat):
+    return max(torch.eig(mat).eigenvalues.flatten())
+
+
+def inv_square_root(mat):
+    assert type(mat) == np.ndarray
+    return scipy.linalg.inv(scipy.linalg.sqrtm(mat))
+
+def rank(mat):
+    """Effective rank of matrix."""
+    return torch.trace(mat)/l2_norm(mat)
+
+def outer(x, y):
+    return x.unsqueeze(1) @ y.unsqueeze(0)
+
+def to_scalar(x):
+    if hasattr(x, 'item'):
+        return x.item()
+    x = to_numpy(x).flatten()
+    assert len(x) == 1
+    return x[0]
+
+def to_numpy(x, dtype=np.float32):
+    """Utility function to convert object to numpy array."""
+    if hasattr(x, 'numpy'):  # PyTorch tensor
+        return x.detach().numpy().astype(dtype)
+    elif type(x) == np.ndarray:
+        return x.astype(dtype)
+    else:                    # Some Python type
+        return np.array(x).astype(dtype)
+
+
+def check_close(observed, truth):
+    truth = to_numpy(truth)
+    observed = to_numpy(observed)
+    assert truth.shape == observed.shape, f"Observed shape {observed.shape}, expected shape {truth.shape}"
+    np.testing.assert_allclose(truth, observed, atol=1e-5, rtol=1e-5)
 
 
 if __name__ == '__main__':
