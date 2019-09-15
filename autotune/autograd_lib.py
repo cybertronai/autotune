fix numerical stability issue in computation of rho
- generate golden set of values to use for factored computation checks

autotune/autograd_lib.py
@@ -32,6 +32,10 @@ B, backprops: backprop values (aka Lop aka Jacobian-vector product) for current 
 
 weight: matmul part of layer, Linear [di, do], Conv [do, di, Kh, Kw]
 
+H, hess  -- hessian
+S, sigma -- noise
+L, lyap -- lyapunov matrix
+
 """
 
 from typing import List
@@ -42,6 +46,7 @@ import torch.nn.functional as F
 
 import util as u
 import globals as gl
+from attrdict import AttrDefault
 
 _supported_layers = ['Linear', 'Conv2d']  # Supported layer class types  TODO(y): make non-private
 _supported_methods = ['exact', 'kron', 'mean_kron', 'experimental_kfac']  # supported approximation methods
@@ -236,7 +241,7 @@ def compute_hess(model: nn.Module, method='exact', attr_name=None) -> None:
       kron: kronecker product
       mean_kron: mean of kronecker products, one kronecker product per datapoint
       experimental_kfac: experimental method for Conv2d
-    field_name: If None, will save hessian to "hess" for exact computation and to "hess_factored" for factored, otherwise use this attr name
+    attr_name: If None, will save hessian to "hess" for exact computation and to "hess_factored" for factored, otherwise use this attr name
 
     Must be called after backprop_hess().
     """
@@ -430,3 +435,255 @@ def backprop_hess(output: torch.Tensor, hess_type: str) -> None:
 
     _global_backprops_prefix = old_backprops_prefix
 
+
+def compute_stats(model):
+    """Combines activations and backprops to compute statistics for a model."""
+
+    # obtain n
+    n = 0
+    for param in model.modules():
+        if hasattr(param, 'activations'):
+            n = param.activations.shape[0]
+            break
+    assert n, "Couldn't figure out size of activations"
+
+    for (i, layer) in enumerate(model.layers):
+
+        if hasattr(layer, 'expensive'):
+            continue
+
+        param_names = {layer.weight: "weight", layer.bias: "bias"}
+        for param in [layer.weight, layer.bias]:
+
+            if param is None:
+                continue
+
+            s = AttrDefault(str, {})  # dictionary-like object for layer stats
+
+            #############################
+            # Gradient stats
+            #############################
+            A_t = layer.activations
+            B_t = layer.backprops_list[0] * n
+
+            s.sparsity = torch.sum(layer.output <= 0) / layer.output.numel()  # proportion of activations that are zero
+            s.mean_activation = torch.mean(A_t)
+            s.mean_backprop = torch.mean(B_t)
+
+            # empirical Fisher
+            G = param.grad1.reshape((n, -1))
+            g = G.mean(dim=0, keepdim=True)
+
+            u.nan_check(G)
+            with u.timeit(f'sigma-{i}'):
+                efisher = G.t() @ G / n
+                sigma = efisher - g.t() @ g
+                s.sigma_l2 = u.sym_l2_norm(sigma)
+                s.sigma_erank = torch.trace(sigma) / s.sigma_l2
+
+            H = param.hess
+
+            u.nan_check(H)
+
+            with u.timeit(f"H_l2-{i}"):
+                s.H_l2 = u.sym_l2_norm(H)
+
+            with u.timeit(f"norms-{i}"):
+                s.H_fro = H.flatten().norm()
+                s.grad_fro = g.flatten().norm()
+                s.param_fro = param.data.flatten().norm()
+
+            # TODO(y): col vs row fix
+            def loss_direction(dd: torch.Tensor, eps):
+                """
+
+                Args:
+                    dd: direction, as a n-by-1 matrix
+                    eps: scalar length
+
+                Returns:
+                   loss improvement if we take step eps in direction dd.
+                """
+                assert u.is_row_matrix(dd)
+                return u.to_scalar(eps * (dd @ g.t()) - 0.5 * eps ** 2 * dd @ H @ dd.t())
+
+            def curv_direction(dd: torch.Tensor):
+                """Curvature in direction dd (directional eigenvalue). """
+                assert u.is_row_matrix(dd)
+                return u.to_scalar(dd @ H @ dd.t() / (dd.flatten().norm() ** 2))
+
+            with u.timeit(f"pinvH-{i}"):
+                pinvH = u.pinv(H)
+
+            with u.timeit(f'curv-{i}'):
+                s.grad_curv = curv_direction(g)  # curvature (eigenvalue) in direction g
+                ndir = g @ pinvH  # newton direction (TODO(y): replace with lstsqsolve)
+                s.newton_curv = curv_direction(ndir)
+                setattr(layer.weight, 'pre', pinvH)  # save Newton preconditioner
+                s.step_openai = 1 / s.grad_curv if s.grad_curv else 1234567
+                s.step_div_inf = 2 / s.H_l2  # divegent step size for batch_size=infinity
+                s.step_div_1 = torch.tensor(2) / torch.trace(H)  # divergent step for batch_size=1
+
+                s.newton_fro = ndir.flatten().norm()  # frobenius norm of Newton update
+                s.regret_newton = u.to_scalar(g @ pinvH @ g.t() / 2)  # replace with "quadratic_form"
+                s.regret_gradient = loss_direction(g, s.step_openai)
+
+            # todo: Lyapunov has to be redone
+            with u.timeit(f'rho-{i}'):
+                s.rho, s.lyap_erank, L_evals = u.truncated_lyapunov_rho(H, sigma)
+                s.step_div_1_adjusted = s.step_div_1 / s.rho
+
+            with u.timeit(f"batch-{i}"):
+                s.batch_openai = torch.trace(H @ sigma) / (g @ H @ g.t())
+                s.diversity = torch.norm(G, "fro") ** 2 / torch.norm(g) ** 2 / n  # Gradient diversity / n
+                s.noise_variance_pinv = torch.trace(pinvH @ sigma)   # todo(y): replace with lsqtsolve
+                s.H_erank = torch.trace(H) / s.H_l2
+                s.batch_jain_simple = 1 + s.H_erank
+                s.batch_jain_full = 1 + s.rho * s.H_erank
+
+            param_name = f"{layer.name}={param_names[param]}"
+            u.log_scalars(u.nest_stats(f"{param_name}", s))
+
+            H_evals = u.symeig_pos_evals(H)
+            S_evals = u.symeig_pos_evals(sigma)
+
+            #s.H_evals = H_evals
+            #s.S_evals = S_evals
+            #s.L_evals = L_evals
+
+            setattr(param, 'stats', s)
+
+            #u.log_spectrum(f'{param_name}/hess', H_evals)
+            #u.log_spectrum(f'{param_name}/sigma', S_evals)
+            #u.log_spectrum(f'{param_name}/lyap', L_evals)
+
+    return None
+
+
+def compute_stats_factored(model):
+    """Combines activations and backprops to compute statistics for a model."""
+
+    # obtain n
+    n = 0
+    for param in model.modules():
+        if hasattr(param, 'activations'):
+            n = param.activations.shape[0]
+            break
+    assert n, "Couldn't figure out size of activations"
+
+    for (i, layer) in enumerate(model.layers):
+
+        if hasattr(layer, 'expensive'):
+            continue
+
+        param_names = {layer.weight: "weight", layer.bias: "bias"}
+        for param in [layer.weight, layer.bias]:
+
+            if param is None:
+                continue
+
+            s = AttrDefault(str, {})  # dictionary-like object for layer stats
+
+            #############################
+            # Gradient stats
+            #############################
+            A_t = layer.activations
+            B_t = layer.backprops_list[0] * n
+
+            AA = A_t @ A_t.t()
+            BB = B_t @ B_t.t()
+
+            # kronecker factored hess and sigma
+            Hk = param.hess_kron
+            Sk = u.KronFactored(AA, BB)
+
+            s.sparsity = torch.sum(layer.output <= 0) / layer.output.numel()  # proportion of activations that are zero
+            s.mean_activation = torch.mean(A_t)
+            s.mean_backprop = torch.mean(B_t)
+
+            # empirical Fisher
+            G = param.grad1.reshape((n, -1))
+            g = G.mean(dim=0, keepdim=True)
+
+            u.nan_check(G)
+            with u.timeit(f'sigma-{i}'):
+                efisher = G.t() @ G / n
+                sigma = efisher - g.t() @ g
+                s.sigma_l2 = u.sym_l2_norm(sigma)
+                s.sigma_erank = torch.trace(sigma) / s.sigma_l2
+
+            H = param.hess
+
+            u.nan_check(H)
+
+            with u.timeit(f"H_l2-{i}"):
+                s.H_l2 = u.sym_l2_norm(H)
+
+            with u.timeit(f"norms-{i}"):
+                s.H_fro = H.flatten().norm()
+                s.grad_fro = g.flatten().norm()
+                s.param_fro = param.data.flatten().norm()
+
+            # TODO(y): col vs row fix
+            def loss_direction(dd: torch.Tensor, eps):
+                """
+
+                Args:
+                    dd: direction, as a n-by-1 matrix
+                    eps: scalar length
+
+                Returns:
+                   loss improvement if we take step eps in direction dd.
+                """
+                assert u.is_row_matrix(dd)
+                return u.to_scalar(eps * (dd @ g.t()) - 0.5 * eps ** 2 * dd @ H @ dd.t())
+
+            def curv_direction(dd: torch.Tensor):
+                """Curvature in direction dd (directional eigenvalue). """
+                assert u.is_row_matrix(dd)
+                return u.to_scalar(dd @ H @ dd.t() / (dd.flatten().norm() ** 2))
+
+            with u.timeit(f"pinvH-{i}"):
+                pinvH = u.pinv(H)
+
+            with u.timeit(f'curv-{i}'):
+                s.grad_curv = curv_direction(g)  # curvature (eigenvalue) in direction g
+                ndir = g @ pinvH  # newton direction (TODO(y): replace with lstsqsolve)
+                s.newton_curv = curv_direction(ndir)
+                setattr(layer.weight, 'pre', pinvH)  # save Newton preconditioner
+                s.step_openai = 1 / s.grad_curv if s.grad_curv else 1234567
+                s.step_div_inf = 2 / s.H_l2  # divegent step size for batch_size=infinity
+                s.step_div_1 = torch.tensor(2) / torch.trace(H)  # divergent step for batch_size=1
+
+                s.newton_fro = ndir.flatten().norm()  # frobenius norm of Newton update
+                s.regret_newton = u.to_scalar(g @ pinvH @ g.t() / 2)  # replace with "quadratic_form"
+                s.regret_gradient = loss_direction(g, s.step_openai)
+
+            # todo: Lyapunov has to be redone
+            with u.timeit(f'rho-{i}'):
+                s.rho, s.lyap_erank, L_evals = u.truncated_lyapunov_rho(H, sigma)
+                s.step_div_1_adjusted = s.step_div_1 / s.rho
+
+            with u.timeit(f"batch-{i}"):
+                s.batch_openai = torch.trace(H @ sigma) / (g @ H @ g.t())
+                s.diversity = torch.norm(G, "fro") ** 2 / torch.norm(g) ** 2 / n  # Gradient diversity / n
+                s.noise_variance_pinv = torch.trace(pinvH @ sigma)   # todo(y): replace with lsqtsolve
+                s.H_erank = torch.trace(H) / s.H_l2
+                s.batch_jain_simple = 1 + s.H_erank
+                s.batch_jain_full = 1 + s.rho * s.H_erank
+
+            param_name = f"{layer.name}={param_names[param]}"
+            u.log_scalars(u.nest_stats(f"{param_name}", s))
+
+            H_evals = u.symeig_pos_evals(H)
+            S_evals = u.symeig_pos_evals(sigma)
+
+            #s.H_evals = H_evals
+            #s.S_evals = S_evals
+            #s.L_evals = L_evals
+
+            setattr(param, 'stats', s)
+
+            #u.log_spectrum(f'{param_name}/hess', H_evals)
+            #u.log_spectrum(f'{param_name}/sigma', S_evals)
+            #u.log_spectrum(f'{param_name}/lyap', L_evals)

autotune/lyapunov_test.py
@@ -117,10 +117,11 @@ def test_lyapunov():
     H = J.t() @ J / n
     noise_variance = torch.trace(H.inverse() @ sigma)
 
+    # H is not quite symmetric, make it so
+    H = H + H.t()
+
     # Slow way
-    # p_sigma = (u.kron(H, torch.eye(d)) + u.kron(torch.eye(d), H)).inverse() @ u.vec(sigma)
-    p_sigma = torch.solve(u.vec(sigma), u.kron(H, torch.eye(d)) + u.kron(torch.eye(d), H))[0]
-    p_sigma = u.unvec(p_sigma, d)
+    p_sigma = u.lyapunov_lstsq(H, sigma)
 
     sigma0 = u.to_numpy(sigma)
     H0 = u.to_numpy(H)
@@ -130,11 +131,51 @@ def test_lyapunov():
     print(f"Error 1: {np.max(abs(H0 @ p_sigma2 + p_sigma2 @ H0 - sigma0))}")
     u.check_close(p_sigma, p_sigma2)
 
-    # alternative even faster way
+    # alternative through SVD
     p_sigma3 = lyapunov_svd(torch.tensor(H0), torch.tensor(sigma0))
     u.check_close(p_sigma2, p_sigma3)
 
-    print("tests passed")
+    # alternative through evals
+    p_sigma4 = u.lyapunov_spectral(torch.tensor(H0), torch.tensor(sigma0))
+    u.check_close(p_sigma2, p_sigma4)
+
+
+def test_stability():
+    bad_sigmas = torch.load('test/bad_sigmas.pt')
+    H = bad_sigmas['H']
+    sigma = bad_sigmas['sigma']
+
+    X = u.lyapunov_spectral(H, sigma)
+    discrepancy = torch.max(abs(X - X.t()) / X)
+    assert discrepancy < 0.01
+
+
+def compare_impl():
+    """
+    lstsq : error 2.23668351395645e-07 erank 4.248441219329834 discrepancy 4760.62841796875
+    scipy : error 4.3144709138687176e-07 erank 5.015336036682129 discrepancy 0.38698798418045044
+    svd : error 2.6252257612213725e-07 erank 5.567287921905518 discrepancy 11.784327507019043
+    spectral : error 3.1967988434189465e-07 erank 4.978618144989014 discrepancy 0.0020115238148719072
+    """
+
+    bad_sigmas = torch.load('test/bad_sigmas.pt')
+    H = bad_sigmas['H']
+    sigma = bad_sigmas['sigma']
+
+    # X = u.lyapunov_spectral(H, sigma)
+
+    def print_stats(tag, X):
+        error = torch.norm(H @ X + X @ H - sigma)
+        discrepancy = torch.max(abs(X - X.t()) / X)
+        print(tag, ": error", error.item(), "erank", u.erank(X).item(), 'discrepancy', discrepancy.item())
+
+    H0, sigma0 = u.to_numpys(H, sigma)
+    X0 = scipy.linalg.solve_lyapunov(H0, sigma0)
+
+    print_stats('lstsq', u.lyapunov_lstsq(H, sigma))
+    print_stats('scipy', torch.tensor(X0))
+    print_stats('svd', u.lyapunov_svd(H, sigma))
+    print_stats('spectral', u.lyapunov_spectral(H, sigma))
 
 
 def lyapunov_svd(A, C, rtol=1e-4, use_svd=False):
@@ -199,4 +240,5 @@ def print_cpu_info():
 
 
 if __name__ == '__main__':
-    u.run_all_tests(sys.modules[__name__])
+    test_stability()
+    #    u.run_all_tests(sys.modules[__name__])

autotune/mnist_end2end_test.py
@@ -293,22 +293,12 @@ def test_main():
                 s.regret_gradient = loss_direction(g, s.step_openai)
 
             with u.timeit(f'rho-{i}'):
-                p_sigma = u.lyapunov_svd(H, sigma)
-                if u.has_nan(p_sigma) and args.compute_rho:  # use expensive method
-                    print('using expensive method')
-                    import pdb;
-                    pdb.set_trace()
-                    H0, sigma0 = u.to_numpys(H, sigma)
-                    p_sigma = scipy.linalg.solve_lyapunov(H0, sigma0)
-                    p_sigma = torch.tensor(p_sigma).to(gl.device)
+                p_sigma = u.lyapunov_spectral(H, sigma)
 
-                if u.has_nan(p_sigma):
-                    # import pdb; pdb.set_trace()
-                    s.psigma_erank = H.shape[0]
-                    s.rho = 1
-                else:
-                    s.psigma_erank = u.sym_erank(p_sigma)
-                    s.rho = H.shape[0] / s.psigma_erank
+                discrepancy = torch.max(abs(p_sigma - p_sigma.t()) / p_sigma)
+
+                s.psigma_erank = u.sym_erank(p_sigma)
+                s.rho = H.shape[0] / s.psigma_erank
 
             with u.timeit(f"batch-{i}"):
                 s.batch_openai = torch.trace(H @ sigma) / (g @ H @ g.t())

autotune/util.py
@@ -30,6 +30,7 @@ u = sys.modules[__name__]
 
 def get_condition(dtype):
     """Return number x such that values below max(eigenval)*x are indistinguishable from noise"""
+    assert 'float' in str(dtype)
     if str(dtype).endswith('float32') or str(dtype).endswith('float16'):
         return 1e3 * 1.1920929e-07
     else:   # assume float64
@@ -242,6 +243,8 @@ def test_l2_norm():
 def sym_l2_norm(mat: torch.Tensor):
     """Largest eigenvalue assuming that matrix is symmetric."""
 
+    u.check_symmetric(mat)
+
     if gl.debug_linalg_crashes:
         try:
             evals, _evecs = torch.symeig(mat)
@@ -288,6 +291,27 @@ def regularize_mat(mat, eps):
     rtol = l2_norm(mat) * eps
     atol = 1e-12
     return mat + torch.eye(mat.shape[0]) * (rtol + atol)
+
+
+def lyapunov_spectral(A, B, cond=None):
+    u.check_symmetric(A)
+    u.check_symmetric(B)
+
+    s, U = torch.symeig(A, eigenvectors=True)
+    if cond is None:
+        cond = get_condition(s.dtype)
+    cutoff = cond * max(s)
+    s = torch.where(s > cutoff, s, torch.tensor(0.))
+
+    C = U.t() @ B @ U
+    s = s.unsqueeze(1) + s.unsqueeze(0)
+    si = torch.where(s > 0, 1/s, s)
+    Y = C * si
+    X = U @ Y @ U.t()
+
+    # cancel small asymetries introduces by multiplication by small numbers
+    X = (X + X.t())/2
+    return X
 
 
 def lyapunov_svd(A, C, rtol=1e-4, eps=1e-7, use_svd=False):
@@ -391,7 +415,7 @@ def lyapunov_lstsq(A, C):
     """Slow explicit solution to least squares Lyapunov in kronecker expanded form."""
     n, n = A.shape
     ii = torch.eye(n)
-    sol = torch.lstsq(u.vec(C), kron(A, ii) + kron(ii, A))[0]
+    sol = torch.lstsq(u.vec(C), kron(ii, A) + kron(A.t(), ii))[0]
     return unvec(sol, n)
 
 
@@ -570,11 +594,11 @@ def pinv_square_root(mat: torch.Tensor, eps=1e-4) -> torch.Tensor:
     return u @ torch.diag(si) @ v.t()
 
 
-def symeig_pos_evals(mat):
-    """Returns positive eigenvalues from symeig in reversed order (decreasing order, to match order of .svd())"""
+def symeig_pos_evals(mat: torch.Tensor) -> torch.Tensor:
+    """Returns positive eigenvalues from symeig in decreasing order (to match order of .svd())"""
 
     s, u = torch.symeig(mat, eigenvectors=False)
-    return torch.flip(filter_evals(s), dims=[0])
+    return torch.flip(filter_evals(s, remove_negative=True), dims=[0])
 
 
 def svd_pos_svals(mat):
@@ -584,19 +608,27 @@ def svd_pos_svals(mat):
     return filter_evals(S)
 
 
-def filter_evals(vals, cond=None):
-    """Given list of eigenvalues or singular values, filter out values indistinguishable from noise."""
+def filter_evals(vals, cond=None, remove_small=True, remove_negative=True):
+    """Given list of eigenvalues or singular values, remove values indistinguishable from noise and/or small values."""
     if cond is None:
         cond = get_condition(vals.dtype)
     above_cutoff = (abs(vals) > cond * torch.max(abs(vals)))
-    return vals[above_cutoff]
+    if remove_small:
+        vals = vals[above_cutoff]
+    if remove_negative:
+        vals = vals[vals > 0]
+    return vals
 
 
 def symsqrt(mat, cond=None, return_rank=False):
-    """Computes the symmetric square root of a positive semi-definite matrix. Throws away small and negative eigenvalues."""
+    """Computes the symmetric square root of a symmetric matrix. Throws away small and negative eigenvalues."""
 
     nan_check(mat)
     s, u = torch.symeig(mat, eigenvectors=True)
+
+    check_symmetric(mat)
+
+    # todo(y): dedupe with getcond
     cond_dict = {torch.float32: 1e3 * 1.1920929e-07, torch.float64: 1E6 * 2.220446049250313e-16}
 
     if cond in [None, -1]:
@@ -657,6 +689,14 @@ def symsqrt_dist(cov1: torch.Tensor, cov2: torch.Tensor) -> float:
     cov1 = symsqrt_svd(cov1)
     cov2 = symsqrt_svd(cov2)
     return torch.norm(cov1 - cov2).item()
+
+
+def check_symmetric(mat):
+    try:
+        u.check_close(mat, mat.t())
+    except:
+        discrepancy = torch.max(abs(mat-mat.t())/mat)
+        print(f"warning, matrix not symmetric: {discrepancy}")
 
 
 def check_close(a0, b0, rtol=1e-5, atol=1e-8) -> None:
@@ -1883,6 +1923,14 @@ def random_cov_pair(shared_rank, independent_rank, d, n=20, strength=1):
     A = shared + strength * random_cov(independent_rank, d, n)
     B = shared + strength * random_cov(independent_rank, d, n)
     return A, B
+
+
+def is_row_matrix(dd):
+    return len(dd.shape) == 2 and dd.shape[0] == 1
+
+
+def is_col_matrix(dd):
+    return len(dd.shape) == 2 and dd.shape[1] == 1
 
 
 if __name__ == '__main__':

autotune/util_test.py
@@ -193,7 +193,7 @@ def test_lyapunov_lstsq():
     print(X)
     print(torch.svd(X)[1])
     # torch.set_default_dtype(torch.float32)
-    
+
 
 def test_robust_svd():
     mat = np.genfromtxt('test/gesvd_crash.txt', delimiter=",")