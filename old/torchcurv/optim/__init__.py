be0cc50b42271a1245878314147120b239d28955
Yaroslav Bulatov <yaroslavvb@gmail.com>
2020-09-23 16:08:52 -0700

    cleanup

--- old/torchcurv/optim/__init__.py
+++
@@ -1,4 +0,0 @@
-from torchcurv.optim.firstorder import DistributedFirstOrderOptimizer  # NOQA
-from torchcurv.optim.secondorder import SecondOrderOptimizer, DistributedSecondOrderOptimizer  # NOQA
-from torchcurv.optim.vi import VIOptimizer, DistributedVIOptimizer  # NOQA
-from torchcurv.optim import lr_scheduler  # NOQA
--- old/torchcurv/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from torchcurv.utils.logger import Logger  # NOQA
-from torchcurv.utils.inv_cupy import inv  # NOQA
-from torchcurv.utils.cholesky_cupy import cholesky  # NOQA
-from torchcurv.utils.accumulator import TensorAccumulator  # NOQA
